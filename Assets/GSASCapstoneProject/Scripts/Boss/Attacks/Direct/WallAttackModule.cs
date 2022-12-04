b7d1be0f03b940a0c6994bf47d44b1cceca996bd
DennisChen11214 <87625335+DennisChen11214@users.noreply.github.com>
2022-12-03 19:14:40 -0500

    added art in

--- Assets/GSASCapstoneProject/Scripts/Boss/Attacks/Direct/WallAttackModule.cs
+++ Assets/GSASCapstoneProject/Scripts/Boss/Attacks/Direct/WallAttackModule.cs
@@ -4,42 +4,67 @@ using System.Collections.Generic;
 public class WallAttackModule : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> _thinWallsPool;
+    private Transform _thinWallsPoolParent;
     [SerializeField]
-    private List<GameObject> _thickWallPool;
+    private Transform _thickWallPoolParent;
+    [SerializeField]
+    private int _poolSize;
+    [SerializeField]
+    private GameObject _thinWall;
+    [SerializeField]
+    private GameObject _thickWall;
 
     private Vector3 _thinWallOriginalPos;
     private Vector3 _thickWallOriginalPos;
 
+    private GameObject[] _thinWallsPool;
+    private GameObject[] _thickWallPool;
+
 
     private void Start()
     {
-        _thinWallOriginalPos = _thinWallsPool[0].transform.position;
-        _thickWallOriginalPos = _thickWallPool[0].transform.position;
+        _thinWallOriginalPos = _thinWallsPoolParent.position;
+        _thickWallOriginalPos = _thickWallPoolParent.position;
+        InitializePool();
+    }
+
+    private void InitializePool()
+    {
+        _thinWallsPool = new GameObject[_poolSize];
+        for (int i = 0; i < _poolSize; i++)
+        {
+            _thinWallsPool[i] = Instantiate(_thinWall, Vector3.zero, Quaternion.identity, _thinWallsPoolParent);
+        }
+
+        _thickWallPool = new GameObject[_poolSize];
+        for (int i = 0; i < _poolSize; i++)
+        {
+            _thickWallPool[i] = Instantiate(_thickWall, Vector3.zero, Quaternion.identity, _thickWallPoolParent);
+        }
     }
 
     public void StartAttack(bool thin, bool enraged = false)
     {
         if (thin)
         {
-            for(int i = 0; i < _thinWallsPool.Count; i++)
+            for(int i = 0; i < _thinWallsPool.Length; i++)
             {
-                if (!_thinWallsPool[i].activeSelf)
+                if (!_thinWallsPool[i].gameObject.activeSelf)
                 {
                     _thinWallsPool[i].transform.position = _thinWallOriginalPos;
-                    _thinWallsPool[i].SetActive(true);
+                    _thinWallsPool[i].gameObject.SetActive(true);
                     return;
                 }
             }
         }
         else
         {
-            for (int i = 0; i < _thickWallPool.Count; i++)
+            for (int i = 0; i < _thickWallPool.Length; i++)
             {
-                if (!_thickWallPool[i].activeSelf)
+                if (!_thickWallPool[i].gameObject.activeSelf)
                 {
                     _thickWallPool[i].transform.position = _thickWallOriginalPos;
-                    _thickWallPool[i].SetActive(true);
+                    _thickWallPool[i].gameObject.SetActive(true);
                     return;
                 }
             }
